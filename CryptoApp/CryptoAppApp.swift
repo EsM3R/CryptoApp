e05c9e0ace775f878a2246850e6bbbf874b6d908
Mehmet Vural <105605335+EsM3R@users.noreply.github.com>
2024-03-13 15:39:45 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Mehmet Vural on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
